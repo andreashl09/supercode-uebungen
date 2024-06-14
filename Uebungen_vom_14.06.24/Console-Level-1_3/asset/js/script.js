00a170824c92dffc68b695833c701ff8a283c149
andreashl09 <a9279352@gmail.com>
2024-06-14 11:54:06 +0200

    add exercise console-level-1_3

---
+++ Uebungen_vom_14.06.24/Console-Level-1_3/asset/js/script.js
@@ -0,0 +1,15 @@
+let i = 1;
+let lastName = "Johnson";
+let j = "2";
+let status = true;
+let hello = "hello";
+let helloWorld = "hello";
+
+// Ausgabe in der Console
+
+console.log(typeof i);
+console.log(typeof lastName);
+console.log(typeof j);
+console.log(typeof status);
+console.log(typeof hello);
+console.log(typeof helloWorld);
