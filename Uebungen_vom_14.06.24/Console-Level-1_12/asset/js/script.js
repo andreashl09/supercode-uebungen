7ad76faf39b0d23b38e084cffbbe6402977856ea
andreashl09 <a9279352@gmail.com>
2024-06-14 13:16:39 +0200

    add exercise intro-level-1_12

---
+++ Uebungen_vom_14.06.24/Console-Level-1_12/asset/js/script.js
@@ -0,0 +1,25 @@
+let score = 5 + 5 * 10;
+console.log("Ergebis: " + score);
+
+score = (5 + 5) * 10;
+console.log("Ergebis: " + score);
+
+score = 0;
+score = score + 10;
+console.log("Ergebis: " + score);
+
+score += 10;
+console.log("Ergebis: " + score);
+
+let zahl = 1;
+zahl = zahl + 1;
+console.log("zahl = zahl + 1 => Ergebis: " + zahl);
+
+zahl += 1;
+console.log("zahl += 1 => Ergebis: " + zahl);
+
+zahl++;
+console.log("increment: " + zahl);
+
+zahl--;
+console.log("increment: " + zahl);
