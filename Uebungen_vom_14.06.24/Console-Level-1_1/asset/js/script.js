0f93f7d85ee45c552d6cc8153e367472bed42009
andreashl09 <a9279352@gmail.com>
2024-06-14 11:45:20 +0200

    add exercises console-level-1_1

---
+++ Uebungen_vom_14.06.24/Console-Level-1_1/asset/js/script.js
@@ -0,0 +1,28 @@
+// "Hello World" ist ein String/ Zeichenkette
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+// Gebe Anton und 28 aus
+let firstName = "Anton";
+let age = 28;
+console.log(firstName);
+console.log(age);
+
+//Gebe Trainer:in aus
+let job = "Trainer:in";
+console.log(job);
+
+//Gebe ein boolean aus
+let married = true;
+console.log(married);
+
+//Fasse alle Variablen zusammen und gebe diese in einem Satz aus.
+console.log(
+    firstName +
+        " ist " +
+        age +
+        " Jahre alt, von Beruf " +
+        job +
+        " und ist verheiratet: " +
+        married
+);
