b22778bd40cd6d624bc5be8836dcfcd1fdbcfe43
andreashl09 <a9279352@gmail.com>
2024-06-17 14:52:40 +0200

    add exercise

---
+++ Uebungen_vom_17.06.24/Functions-Level-1_5/asset/js/script.js
@@ -0,0 +1,11 @@
+function math(num1, num2) {
+    console.log(`Mutliplikation von ${num1} und ${num2}: ${num1 * num2}`);
+    console.log(`Division von ${num1} und ${num2}: ${num1 / num2}`);
+}
+
+math(10, 2);
+math(30, 20);
+math(100, 100);
+math(5, 0);
+math(45, 173);
+math(1, 1000);
