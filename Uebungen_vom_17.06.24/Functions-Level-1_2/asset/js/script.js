1e2ef45f3475a4cb1b6e03da23c30c30e2998dc7
andreashl09 <a9279352@gmail.com>
2024-06-17 14:34:48 +0200

    add exercise

---
+++ Uebungen_vom_17.06.24/Functions-Level-1_2/asset/js/script.js
@@ -0,0 +1,8 @@
+let name = "SuperCoder:in";
+
+function intro2(nameSuper, firstName) {
+    console.log(`Hi ${nameSuper}. Mein Name ist ${firstName}.`);
+}
+
+intro2(name, "Andreas");
+intro2(name, "der Dieter");
