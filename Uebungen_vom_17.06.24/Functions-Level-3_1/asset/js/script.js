b549afead811ff642cba2795abe5deb61de20665
andreashl09 <a9279352@gmail.com>
2024-06-17 16:00:59 +0200

    add exercise

---
+++ Uebungen_vom_17.06.24/Functions-Level-3_1/asset/js/script.js
@@ -0,0 +1,19 @@
+const mainBox = document.querySelector("main");
+const h1Html = document.querySelector("h1");
+
+function greenAndPurple() {
+    mainBox.style.backgroundColor = "rgb(49, 156, 96)";
+    h1Html.style.color = "#7b096a";
+}
+function bluepurpleAndYellow() {
+    mainBox.style.backgroundColor = "#504c7d";
+    h1Html.style.color = "#7b096a";
+}
+function blackAndDarkorange() {
+    mainBox.style.backgroundColor = "#020024";
+    h1Html.style.color = "#cf6b0b";
+}
+function blueAndDarkblue() {
+    mainBox.style.backgroundColor = "#050351";
+    h1Html.style.color = "#42d4ff";
+}
