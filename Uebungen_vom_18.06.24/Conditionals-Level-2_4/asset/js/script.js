5488323461f379f4f924fc5ee4f73090c054dbe3
andreashl09 <a9279352@gmail.com>
2024-06-18 12:13:55 +0200

    add exercise

---
+++ Uebungen_vom_18.06.24/Conditionals-Level-2_4/asset/js/script.js
@@ -0,0 +1,12 @@
+function summe(num1, num2) {
+    console.log(num1);
+    console.log(num2);
+
+    if (num1 === num2) {
+        console.log(`Summe: ${num1 + num2}`);
+    } else {
+        console.log("Die Zahlen sind nicht gleich");
+    }
+}
+
+summe(2.0, 2.5);
